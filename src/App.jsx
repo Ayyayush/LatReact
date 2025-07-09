60ecf66940466870d990b0fedd2a1fb4b9e3160f
Ayush <140370865+Ayyayush@users.noreply.github.com>
2025-07-09 08:04:47 +0530

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,28 @@
-import dragonImage from "./assets/l_how-to-train-your-dragon-movie-poster_58ada901.jpg";
+
+import Netflix from "./components/NetflixSeries.jsx";
 
 export const App = () => {
   return (
-    < div style={{ fontFamily: "Arial, sans-serif", color: "#333", backgroundColor: "#f0f0f0" ,marginTop: "0px"}}>
+    <div
+      style={{
+        fontFamily: "Arial, sans-serif",
+        color: "#333",
+        backgroundColor: "#f0f0f0",
+        marginTop: "0px",
+      }}
+    >
+      {/* This is arrows and key way of returning multiple elements */}
+      {/* return ([<Netflix key="1" />,
+        <Netflix key="2" />,
+        <Netflix key="3" />,
+        <Netflix key="4" />,]) */}
 
-     
       <Netflix />
-       <Netflix />
-        <Netflix />
-         <Netflix />
-          <Netflix />
-
-     
+    
     </div>
   );
 };
 
 
-const Netflix = () => {
-  return (
-   
-      <div style={{ textAlign: "center", padding: "20px" }}>
-        <img src={dragonImage} alt="How to Train your Dragon" width="80%" />
-        <h1>A recent Movie I Watched</h1>
-        <h2>How to Train your Dragon</h2>
-        <p>
-          Summary: A young Viking named Hiccup befriends a feared dragon,
-          Toothless, and together they change their world by proving that
-          dragons and humans can live in harmony.
-        </p>
-      </div>
-  );
-}
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Practices } from '../0-React/7-ReactInterview'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
